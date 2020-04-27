66f1096860699db6e8a33e650d5edaeda262e3d3
Michal Dudkiewicz <michal.dudkiewicz96@gmail.com>
2020-04-27 13:54:31 +0200

    target removed

--- cmake-build-debug/library/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student/test/library
-# Build directory: /home/student/test/cmake-build-debug/library
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(LibraryTest "Test")
-set_tests_properties(LibraryTest PROPERTIES  _BACKTRACE_TRIPLES "/home/student/test/library/CMakeLists.txt;68;add_test;/home/student/test/library/CMakeLists.txt;0;")
